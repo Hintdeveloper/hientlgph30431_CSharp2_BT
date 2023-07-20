577060350ec5f0ff6cc09f06840f82cd36f27aa4
Hintdeveloper <hientranle1209@gmail.com>
2023-07-20 08:57:10 +0700

    Add project files.

---
+++ hientlgph30431_CSharp2_BT/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hientlgph30431_CSharp2_BT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
